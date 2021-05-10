807e4b66d4d0c865d5eab3ad7d240057a1549be0
Jilmer-Mayta-Condori <jilmer.mayta@tecsup.edu.pe>
2021-05-10 16:04:27 -0500

    Buenas Practicas en NodeSJ y DDD

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const apiAgenda = require('./routes/agenda')
+
+const errorHandler = require('./utils/middlewares/errorHandlers')
+const notFoundHandler = require('./utils/middlewares/notFoundHandler')
+
+app.use(express.json())
+
+app.use('/', apiAgenda)
+
+// Middleware Final para rutas inexistentes
+app.use(notFoundHandler)
+
+// Middlewares
+app.use(errorHandler)
+
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { 'Content-Type': 'application/json' })
+//   response.end(JSON.stringify(persons))
+// })
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
